5e7c83a6d132e3a164a185c4470c8d753263ebfa
ripleyaffect <t.ripley.peterson@gmail.com>
2013-10-08 22:33:41 -0400

    Extend Dictionary parser to handle phrases

--- dict_parser.rb
+++ dict_parser.rb
@@ -1,21 +1,24 @@
 # Dictionary parser for bias program.
-# Accepts one entry per line in the form "[WORD] [VALUE]"
+# Accepts one entry per line in the form "[PHRASE]= [VALUE]"
 
 def parse dict_file   
 	file = File.open( dict_file )
 
-	words = []
+	phrases = []
 	dictionary = {}
 	
 	# read in the file and put them into a hash which represents the dictionary
-	file.each{ |line| words << line.split }
-	words.map do |key, value| 
+	file.each{ |line| phrases << line.split('=') }
+	phrases.map do |key, value| 
+		key.strip!
 		value = 0 if value == nil || value == "#"
-		dictionary[key] = value.to_i unless key == '#' 
+		dictionary[key] = value.to_i unless key[0] == '#' 
 	end
 
 	# feedback for the user
 	puts "The dictionary was read"
+
+	puts dictionary
 
 	return dictionary
 end
